7f7968df39d5761cac2f58df3d02a99ba527d22c
marco-vit-pagopa <marco.vit@pagopa.it>
2024-09-15 23:45:12 +0200

    pn-12570 : WIP : aggiunto filtro per paId e uplod zip finale

--- pn-12570/java_code/split_con020/scripts/prepare_output_zip/prepare_zip_from_output_queue_dump.sh
+++ pn-12570/java_code/split_con020/scripts/prepare_output_zip/prepare_zip_from_output_queue_dump.sh
@@ -12,11 +12,12 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 safestorage_api=http://localhost:9090/
 aws_profile=sso_pn-core-dev
-aws_region=eu-central-1
+aws_region=eu-south-1
 
-bucket_name_and_base_path=s3://cf-templates-1rnjss1bg2rvu-eu-central-1/zip_test/
+bucket_name_and_base_path=s3://mvit-prove-aws-emr-parquet-830192246553-001/zip_test/
 sqs_dump_name=dump_pn-paper-event-enrichment-output_2024-09-13T15-44-41-638Z.json
 
+selectedPaId="5b994d4a-0fa8-47ac-9c7b-354f1d44a1ce"
 
 aws_command_base_args=""
 if ( [ ! -z "${aws_profile}" ] ) then
@@ -48,19 +49,31 @@ do
   registeredLetterCode=$( echo $json_line | jq -r '.registeredLetterCode')
   printedPdf=$( echo $json_line | jq -r '.printedPdf')
 
-  csv_line="${iun};${recIndex};${sendRequestId};${generationTime};${eventTime};${registeredLetterCode};${printedPdf}"
-  echo "Msg: $csv_line"
-  echo "$csv_line" >> ${tmp_dir}/zip_content/index.csv
+  paId=$( aws $aws_command_base_args dynamodb get-item --table-name pn-Notifications \
+      --key '{ "iun": {"S": "'${iun}'"} }' | jq -r '.Item.senderPaId.S' )
+
+  if ( [ "$paId" = "$selectedPaId" ] ) then
+    csv_line="${iun};${recIndex};${sendRequestId};${generationTime};${eventTime};${registeredLetterCode};${printedPdf}"
+    echo "Msg: $csv_line"
+    echo "$csv_line" >> ${tmp_dir}/zip_content/index.csv
 
 
-  curl -X GET \
-    -H "x-pagopa-safestorage-cx-id: pn-test" \
-    ${safestorage_api}safe-storage/v1/files/${printedPdf} \
-      | tee > ${tmp_dir}/out.txt
+    curl -X GET \
+      -H "x-pagopa-safestorage-cx-id: pn-test" \
+      ${safestorage_api}safe-storage/v1/files/${printedPdf} \
+        | tee > ${tmp_dir}/out.txt
 
-  presigned_url=$( cat ${tmp_dir}/out.txt | jq -r ".download.url")
-  echo "Download pdf from ${presigned_url}"
-  curl ${presigned_url} > ${tmp_dir}/zip_content/${printedPdf}
+    presigned_url=$( cat ${tmp_dir}/out.txt | jq -r ".download.url")
+    echo "Download pdf from ${presigned_url}"
+    curl ${presigned_url} > ${tmp_dir}/zip_content/${printedPdf}
+  else
+    echo "IUN $iun sended by PA-Id: $paId discarded"
+  fi
 
 done < <( cat ${tmp_dir}/sqs_dump.json | jq -r '.[] | .Body | fromjson | tojson ')
+
+( cd ${tmp_dir}/zip_content/ && zip -r ../con020.zip * )
+
+zip_end_name=$( echo $sqs_dump_name | sed -e 's/.json$/_con020.zip/' )
+aws $aws_command_base_args s3 cp "${tmp_dir}/con020.zip"  "${bucket_name_and_base_path}${zip_end_name}"
 
