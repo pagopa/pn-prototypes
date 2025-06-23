07c2f82269afe56f7d82ace971598be7129bf7c3
francesca-restante <francesca.restante@external.pagopa.it>
2025-06-23 17:21:34 +0200

    Feature/pn 14180 (#24)
    * Update matriceCostiPivot.sql

--- analog-delivery-monitoring/source-views/matriceCostiPivot.sql
+++ analog-delivery-monitoring/source-views/matriceCostiPivot.sql
@@ -80,6 +80,23 @@ OPTIONS (
 /*
 $QueryMetadata
 {
+    "name": "matrice_costi_202506",
+    "persist": false,
+    "dependencies": []
+}
+*/
+CREATE OR REPLACE TEMPORARY VIEW matrice_costi_202506
+USING csv
+OPTIONS (
+  path   "s3a://${CORE_BUCKET}/external/matrice_costi/matrice_costi_20250616_v1.0.csv.gz",
+  delimiter ";",
+  header true
+);
+
+
+/*
+$QueryMetadata
+{
     "name": "matrice_costi",
     "persist": false,
     "dependencies": [
@@ -101,6 +118,11 @@ $QueryMetadata
         },
         {
             "name": "matrice_costi_202504",
+            "location": "analog-delivery-monitoring/source-views/matriceCostiPivot.sql"
+
+        },
+        {
+            "name": "matrice_costi_202506",
             "location": "analog-delivery-monitoring/source-views/matriceCostiPivot.sql"
         }
     ]
@@ -173,7 +195,24 @@ CREATE OR REPLACE TEMPORARY VIEW matrice_costi AS (
             costo, 
             costo_base_20gr, 
             startDate,
-            endDate,
+
+            '2025-06-15T21:59:59.999Z' AS endDate,
             'matrice_costi_202504' AS tenderVersion 
         FROM matrice_costi_202504
+    UNION ALL
+    SELECT  geokey, 
+            product, 
+            recapitista, 
+            lotto, 
+            costo_plico, 
+            costo_foglio, 
+            costo_demat, 
+            min, 
+            max, 
+            costo, 
+            costo_base_20gr, 
+            startDate,
+            endDate,
+            'matrice_costi_202506' AS tenderVersion 
+        FROM matrice_costi_202506
 );
